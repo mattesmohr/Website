bb063cbb1977befb8ac7b6ff798022caa77d64e3
Mattes Mohr <mail@mattesmohr.de>
2023-11-18 21:56:17 +0100

    Revise some of the login

--- Sources/Website/Controllers/Areas/LoginAreaController.swift
+++ Sources/Website/Controllers/Areas/LoginAreaController.swift
@@ -9,29 +9,6 @@ final class LoginAreaController {
         return request.redirect(to: "/area/login/login")
     }
     
-    // [/register]
-    func getRegister(_ request: Request) async throws -> View {
-
-        let viewModel = LoginAreaPageModel.RegisterViewModel()
-        
-        return try await request.htmlkit.render(LoginAreaPage.RegisterView(viewModel: viewModel))
-    }
-    
-    // [/register/:model]
-    func postRegister(_ request: Request) async throws -> Response {
-        
-        try RegisterModel.Input.validate(content: request)
-        
-        let model = try request.content.decode(RegisterModel.Input.self)
-        
-        let digest = try request.password.hash(model.password)
-        
-        try await CredentialRepository(database: request.db)
-            .insert(entity: CredentialEntity(password: digest, role: CredentialModel.Roles.administrator.rawValue, status: CredentialModel.States.unlocked.rawValue), with: UserEntity(email: model.email))
-        
-        return request.redirect(to: "/area/login/login")
-    }
-    
     // [/login]
     func getLogin(_ request: Request) async throws -> View {
         
@@ -47,15 +24,14 @@ final class LoginAreaController {
         
         let model = try request.content.decode(LoginModel.Input.self)
         
-        guard let entity = try await UserRepository(database: request.db).find(name: model.email) else {
-            return request.redirect(to: "/area/login/login")
-        }
+        if let entity = try await UserRepository(database: request.db).find(name: model.email) {
+
+            if try await request.password.async.verify(model.password, created: entity.credential!.password) {
+            
+                request.session.authenticate(UserModel.Output(entity: entity))
         
-        if try! request.password.verify(model.password, created: entity.credential!.password) {
-        
-            request.session.authenticate(UserModel.Output(entity: entity))
-    
-            return request.redirect(to: "/area/admin/home/index")
+                return request.redirect(to: "/area/admin/home/index")
+            }
         }
         
         return request.redirect(to: "/area/login/login")
@@ -69,19 +45,6 @@ final class LoginAreaController {
         
         return request.redirect(to: "/area/login/index")
     }
-    
-    // [/reset]
-    func getReset(_ request: Request) async throws -> View {
-        
-        let viewModel = LoginAreaPageModel.ResetViewModel()
-        
-        return try await request.htmlkit.render(LoginAreaPage.ResetView(viewModel: viewModel))
-    }
-    
-    // [/reset/:model]
-    func postReset(_ request: Request) async throws -> Response {
-        return request.redirect(to: "/area/login/login")
-    }
 }
 
 extension LoginAreaController: RouteCollection {
@@ -91,13 +54,9 @@ extension LoginAreaController: RouteCollection {
         routes.group("login") { routes in
             
             routes.get("index", use: self.getIndex)
-            routes.get("register", use: self.getRegister)
-            routes.post("register", use: self.postRegister)
             routes.get("login", use: self.getLogin)
             routes.post("login", use: self.postLogin)
             routes.get("logout", use: self.getLogout)
-            routes.get("reset", use: self.getReset)
-            routes.post("reset", use: self.postReset)
         }
     }
 }
